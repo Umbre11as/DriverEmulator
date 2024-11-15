7411999818c3df407cd065824790e8f2c2918a86
Umbre11as <olegumbre11a@yandex.ru>
2024-11-15 18:50:44 +0300

    Enable SEH exceptions

--- emulated/ntoskrnl/cmake/flags.cmake
+++ emulated/ntoskrnl/cmake/flags.cmake
@@ -4,8 +4,10 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/dist")
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/dist")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/dist")
 
+add_compile_options("/W0")
 add_compile_options("/Od")
 add_compile_options("/Gy")
+add_compile_options("/EHs") # SEH exceptions
 
 add_link_options("/WX:NO")
 add_link_options("/NOENTRY")

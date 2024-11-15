86b00547aa1586dcfd0154edde58dab1241e0d33
Umbre11as <olegumbre11a@yandex.ru>
2024-11-15 20:23:26 +0300

    Added MmIsAddressValid using VEH Now dll has an entrypoint

--- emulated/ntoskrnl/src/main.c
+++ emulated/ntoskrnl/src/main.c
@@ -607,3 +607,43 @@ EXPORT NTSTATUS RtlFindExportedRoutineByName(IN PVOID BaseOfImage, IN PCSTR Rout
     RtlFindExportedRoutineByNameFn rferbn = (RtlFindExportedRoutineByNameFn) GetProcAddress(ntdll, "RtlFindExportedRoutineByName");
     return rferbn(BaseOfImage, RoutineName);
 }
+
+PVOID LastCheckAddress = NULL;
+BOOLEAN BadAddressCheck = FALSE;
+
+LONG NTAPI VEHandler(IN PEXCEPTION_POINTERS ExceptionInfo) {
+    PEXCEPTION_RECORD record = ExceptionInfo->ExceptionRecord;
+    if (record->ExceptionCode == STATUS_ACCESS_VIOLATION && record->ExceptionAddress == LastCheckAddress) {
+        BadAddressCheck = TRUE;
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+// ReSharper disable CppDFAUnreadVariable
+// ReSharper disable CppDFAConstantFunctionResult
+// ReSharper disable CppDFAUnreachableCode
+// ReSharper disable CppDFAUnusedValue
+// ReSharper disable CppDeclaratorNeverUsed
+EXPORT BOOLEAN MmIsAddressValid(IN PVOID VirtualAddress) {
+    LastCheckAddress = VirtualAddress;
+
+    BYTE unused = *(BYTE*)VirtualAddress;
+    const BOOLEAN result = BadAddressCheck;
+
+    BadAddressCheck = FALSE;
+    return result;
+}
+// ReSharper restore CppDeclaratorNeverUsed
+// ReSharper restore CppDFAUnusedValue
+// ReSharper restore CppDFAUnreachableCode
+// ReSharper restore CppDFAConstantFunctionResult
+// ReSharper restore CppDFAUnreadVariable
+
+BOOL WINAPI DllMain(IN HINSTANCE InstanceHandle, IN DWORD Reason, IN PVOID Reserved) {
+    if (Reason == DLL_PROCESS_ATTACH)
+        AddVectoredExceptionHandler(1, VEHandler);
+
+    return TRUE;
+}

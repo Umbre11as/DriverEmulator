b94bf9737c430409564861c91a44b8a11e66c41b
Umbre11as <olegumbre11a@yandex.ru>
2024-11-17 23:34:19 +0300

    No only ntoskrnl here

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <unordered_map>
 #include "Utils/FileUtils.h"
 #include "PE/PortableExecutable.h"
 
 using DriverEntryFn = NTSTATUS(*)(PVOID, PVOID);
+
+std::unordered_map<std::string, std::string> modulePerModule {
+    { "ntoskrnl.exe", "ntoskrnl.dll" },
+    { "fltmgr.sys", "fltmgr.dll" }
+};
 
 int main() {
     std::vector<BYTE> buffer = Utils::File::Read(R"(..\environment\HelloWorld.sys)");
@@ -13,20 +19,15 @@ int main() {
     PortableExecutable pe(buffer);
 
     PBYTE mapped = pe.Map();
-    HMODULE ntoskrnl = LoadLibrary("ntoskrnl.dll");
 
     if (DWORD importsRVA = pe.NtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress) {
         for (auto importDescriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(mapped + importsRVA); importDescriptor->Characteristics; importDescriptor++) {
-            // ReSharper disable CppTooWideScopeInitStatement
             auto dllName = reinterpret_cast<PCSTR>(mapped + importDescriptor->Name);
-            if (strcmp(dllName, "ntoskrnl.exe") != 0) {
-                std::cerr << "Unsupported target module: " << dllName << std::endl;
-            }
-            // ReSharper restore CppTooWideScopeInitStatement
+            HMODULE module = LoadLibrary(modulePerModule[std::string(dllName)].c_str());
 
             for (auto thunkData = reinterpret_cast<PIMAGE_THUNK_DATA>(mapped + importDescriptor->FirstThunk); thunkData->u1.AddressOfData; thunkData++) {
                 if (auto importByName = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(mapped + thunkData->u1.AddressOfData)) {
-                    thunkData->u1.Function = reinterpret_cast<ULONGPTR>(GetProcAddress(ntoskrnl, importByName->Name));
+                    thunkData->u1.Function = reinterpret_cast<ULONGPTR>(GetProcAddress(module, importByName->Name));
                     if (!thunkData->u1.Function)
                         std::cerr << "Unresolved import: " << importByName->Name << std::endl;
                 }
